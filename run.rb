eac3811fe5af6fc50dcb1d6f21837561be1482a8
Turner Davis <spoonflower@Turner-Davis-MacBook-Pro-0521.local>
2018-06-27 13:38:40 -0400

    add method to detect win

--- run.rb
+++ run.rb
@@ -2,9 +2,19 @@ puts "let the games begin!"
 
 @empty_squares = [0,1,2,3,4,5,6,7,8]
 @filled_squares = Array.new(9)
-
-
+@winner = nil
+@winning_sets = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 def draw
+  puts ""
   3.times do |r|
     row = ""
     3.times do |c|
@@ -17,10 +27,24 @@ end
 def turn(input, player)
   @filled_squares[input] = "#{player} "
   @empty_squares[input] = nil
+  draw
+  detect_winner(player)
+end
+
+def detect_winner(player)
+  @winning_sets.each do |set|
+    values = @filled_squares.values_at(*set)
+    if values == Array.new(3, player)
+      p 'winner'
+      @winner = player
+    end
+  end
 end
 
 draw
-turn(gets.chomp.to_i, "x")
-draw
-turn(@empty_squares.compact.sample, 'o')
-draw   
+while !@winner do
+  turn(gets.chomp.to_i, "x")
+  turn(@empty_squares.compact.sample, 'o')
+end
+
+puts "winner is #{@winner}"
