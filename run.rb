2b60a13242da0fc8a8bdd4b9bdd3c8552934c9e9
Turner Davis <spoonflower@Turner-Davis-MacBook-Pro-0521.local>
2018-06-27 14:33:45 -0400

    add active player and winner dection method

--- run.rb
+++ run.rb
@@ -3,6 +3,7 @@ puts "let the games begin!"
 @empty_squares = [0,1,2,3,4,5,6,7,8]
 @filled_squares = Array.new(9)
 @winner = nil
+@active_player = nil
 @winning_sets = [
   [0,1,2],
   [3,4,5],
@@ -18,24 +19,24 @@ def draw
   3.times do |r|
     row = ""
     3.times do |c|
-      row += @filled_squares[(r*3 + c)] ? @filled_squares[(r*3 + c)] : ". "
+      row += @filled_squares[(r*3 + c)] ? "#{@filled_squares[(r*3 + c)]} " : ". "
     end
     puts row
   end
 end
 
 def turn(input, player)
-  @filled_squares[input] = "#{player} "
+  @filled_squares[input] = player
   @empty_squares[input] = nil
   draw
   detect_winner(player)
+  @active_player = player == 'x' ? 'o': 'x'
 end
 
 def detect_winner(player)
   @winning_sets.each do |set|
     values = @filled_squares.values_at(*set)
     if values == Array.new(3, player)
-      p 'winner'
       @winner = player
     end
   end
@@ -43,8 +44,11 @@ end
 
 draw
 while !@winner do
-  turn(gets.chomp.to_i, "x")
-  turn(@empty_squares.compact.sample, 'o')
+  if @active_player == 'o'
+    turn(@empty_squares.compact.sample, 'o')
+  else
+    turn(gets.chomp.to_i, "x")
+  end
 end
 
 puts "winner is #{@winner}"
